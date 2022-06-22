455b171a81e0d0f57bd9b3efa90b42c50f305880
jvfe <jvfecav@gmail.com>
2022-06-22 16:28:38 -0300

    feat: Add local HTML dependencies
    - Creates embed_local_file function

--- microview/rendering.py
+++ microview/rendering.py
@@ -4,10 +4,21 @@ from typing import Dict
 from jinja2 import Environment, PackageLoader
 
 
+def embed_local_file(filename, filedir="templates"):
+
+    herepath = Path(__file__).parent.resolve()
+    fullpath = herepath.joinpath(filedir, filename)
+
+    with open(fullpath, "r") as f:
+        return f.read()
+
+
 def render_base(tax_plots: Dict) -> None:
     env = Environment(
         loader=PackageLoader("microview", "templates"),
     )
+
+    env.globals["embed_local_file"] = embed_local_file
 
     base_template = env.get_template("base.html")
 
