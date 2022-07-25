0d08b14d7c451a4d34aeaa9d79b68238e65b45ee
jvfe <jvfecav@gmail.com>
2022-07-25 08:23:49 -0300

    refactor: Externalize path validation with df

--- microview/file_finder.py
+++ microview/file_finder.py
@@ -18,13 +18,7 @@ def find_reports(reports_path: Path) -> Tuple[List[Path], str]:
     return report_paths, report_type
 
 
-def parse_source_table(source_table: Path) -> Tuple[List[Path], str]:
-
-    df = pd.read_csv(source_table)
-
-    sample_paths: List[Path] = [Path(sample) for sample in df["sample"].to_list()]
-
-    report_type = detect_report_type(sample_paths)
+def validate_paths(sample_paths: List[Path], source_table: Path) -> List[Path]:
 
     if all(sample_path.exists for sample_path in sample_paths) != True:
         full_sample_paths: List[Path] = [
@@ -34,6 +28,19 @@ def parse_source_table(source_table: Path) -> Tuple[List[Path], str]:
         if all(full_path.exists() for full_path in full_sample_paths) != True:
             raise Exception("One or more provided sample paths doesn't exist")
 
-        return full_sample_paths, report_type
+        return full_sample_paths
 
-    return sample_paths, report_type
+    return sample_paths
+
+
+def parse_source_table(source_table: Path) -> Tuple[List[Path], str]:
+
+    df = pd.read_csv(source_table)
+
+    sample_paths: List[Path] = [Path(sample) for sample in df["sample"].to_list()]
+
+    report_type = detect_report_type(sample_paths)
+
+    validated_paths = validate_paths(sample_paths, source_table)
+
+    return validated_paths, report_type
