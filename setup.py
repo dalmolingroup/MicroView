1202621ad4c6ae6df85ac107e7bf4dca0d0c5887
jvfe <jvfecav@gmail.com>
2022-08-16 15:32:52 -0300

    Bump version: 0.7.1 → 0.8.0

--- setup.py
+++ setup.py
@@ -64,6 +64,6 @@ setup(
         "Documentation": "https://jvfe.github.io/microview/",
         "Source Code": "https://github.com/jvfe/microview",
     },
-    version="0.7.1",
+    version="0.8.0",
     zip_safe=False,
 )
